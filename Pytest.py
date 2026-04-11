9dc1f3c52df9006c678fbf3e80a7645950a7089a
Ayano <fingerdash592@gmail.com>
2026-04-11 21:43:39 +0300

    mid comm

--- Pytest.py
+++ Pytest.py
@@ -1,25 +1,54 @@
 import re
 from pathlib import Path
 
+cfgpath = Path(__file__).parent / "cfg.txt"
+p = Path("C:\\Users\\user")
+
+
 def game_name (name):
     name = name.replace('_', ' ').replace('-', ' ')
-    return re.sub(r'([a-z])([A-Z])', r'\1 \2', name)
+    return re.sub(r'([a-z])([A-Z0-9])', r'\1 \2', name)
+def start():
+    if not cfgpath.exists():
+        x = list(p.glob('**/*.sav'))
+        dic = {}
+        for i in x:
+            s = 0
+            while 'save' in i.parent.name.lower():
+                if i.parent.name != 'Users':
+                    i = i.parent
+                else:
+                    s = 1
+                    break
+            if not s:
+                dic[game_name(i.parent.name)] = i.parent
+        with open(cfgpath, 'w', encoding="utf-8") as f:
+            for i in dic.keys():
+                f.write(i + "\n" + str(dic[i]) + "\n")
+def add_game():
+    print("Enter the name of the game you want to add:")
+    name = game_name(input().replace(" ", ""))
+    print("Enter the path where the .sav files are saved:")
+    way = input().replace(" ", "")
+    with open(cfgpath, 'a', encoding="utf-8") as f:
+            f.write("\n" + name + "\n" + way, )
 
-cfgpath = Path(__file__).parent
-if cfgpath.glob('cfg.txt'):
-    p = Path("C:\\Users\\user")
-    x = list(p.glob('**/*.sav'))
-    dic = {}
-    for i in x:
-        s = 0
-        while 'save' in i.parent.name.lower():
-            if i.parent.name != 'Users':
-                i = i.parent
-            else:
-                s = 1
-                break
-        if not s:
-            dic[game_name(i.parent.name)] = i.parent
-    with open(str(cfgpath) + r"\cfg.txt", 'w', encoding="utf-8") as f:
-        for i in dic.keys():
-            f.write(i + "\n" + str(dic[i]) + "\n")
+def delete_game():
+    if not cfgpath.exists():
+        print("Файл настроек не найден.")
+        return
+    print("Enter the name of the game you want to delete:")
+    name = game_name(input().replace(" ", ""))
+    skip = False
+    new_data = []
+    for i in cfgpath.read_text().splitlines():
+        if skip:
+            skip = False
+            continue
+        if i == name:
+            skip = True
+            continue
+        new_data.append(i)
+    res = [i for i in new_data]
+    cfgpath.write_text("\n".join (res), encoding="utf-8")
+
